47a855c9044ef8c96164a3be9058839840beecb8
IsseNisse <isak.vindeln@gmail.com>
2021-04-23 09:57:08 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void button(ActionEvent actionEvent) {
+        System.out.println("Hello world");
+    }
+}
